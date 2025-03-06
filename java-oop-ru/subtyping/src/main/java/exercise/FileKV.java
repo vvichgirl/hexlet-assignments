bd8f14fd2c72231590648bc571aa9d8743d3e829
@hexlet/cli <support@hexlet.io>
2025-03-06 14:50:02 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
+++ java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
@@ -6,36 +6,36 @@ import java.util.Map;
 // BEGIN
 public class FileKV implements KeyValueStorage {
     private String filePath;
-    private Map<String, String> storage;
 
     FileKV(String filePath, Map<String, String> storage) {
         this.filePath = filePath;
-        this.storage = new HashMap<>(storage);
-        Utils.writeFile(filePath, Utils.serialize(this.storage));
+        Utils.writeFile(filePath, Utils.serialize(storage));
     }
 
     public void set(String key, String value) {
-        storage.put(key, value);
-        Utils.writeFile(filePath, Utils.serialize(this.storage));
+        var data = Utils.readFile(filePath);
+        var dataMap = Utils.deserialize(data);
+        dataMap.put(key, value);
+        Utils.writeFile(filePath, Utils.serialize(dataMap));
     }
 
     public void unset(String key) {
         var data = Utils.readFile(filePath);
-        storage = Utils.deserialize(data);
-        storage.remove(key);
-        Utils.writeFile(filePath, Utils.serialize(storage));
+        var dataMap = Utils.deserialize(data);
+        dataMap.remove(key);
+        Utils.writeFile(filePath, Utils.serialize(dataMap));
     }
 
     public String get(String key, String defaultValue) {
         var data = Utils.readFile(filePath);
-        storage = Utils.deserialize(data);
-        return storage.getOrDefault(key, defaultValue);
+        var dataMap = Utils.deserialize(data);
+        return dataMap.getOrDefault(key, defaultValue);
     }
 
     public Map<String, String> toMap() {
         var data = Utils.readFile(filePath);
-        storage = Utils.deserialize(data);
-        return new HashMap<>(storage);
+        var dataMap = Utils.deserialize(data);
+        return new HashMap<>(dataMap);
     }
 }
 // END
