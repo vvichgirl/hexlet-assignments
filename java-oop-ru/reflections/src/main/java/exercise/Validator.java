c45272562a1b503989f485e21287142bdd474e78
@hexlet/cli <support@hexlet.io>
2025-03-09 21:27:56 +0300

    submit java-oop-ru/reflections

--- java-oop-ru/reflections/src/main/java/exercise/Validator.java
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -7,58 +7,68 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Validator {
     public static List<String> validate(Object obj) {
-        List<String> result = new ArrayList<>();
+        Class<?> aClass = obj.getClass();
 
-        try {
-            Class<?> aClass = obj.getClass();
-            Field[] declaredFields = aClass.getDeclaredFields();
-
-            for (Field field : declaredFields) {
-                field.setAccessible(true);
-                if (field.isAnnotationPresent(NotNull.class) && Objects.isNull(field.get(obj))) {
-                    result.add(field.getName());
-                }
-            }
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-
-        return result;
+        return List.of(aClass.getDeclaredFields()).stream()
+                .filter((field) -> {
+                    var isNull = false;
+                    try {
+                        field.setAccessible(true);
+                        isNull = field.isAnnotationPresent(NotNull.class) && Objects.isNull(field.get(obj));
+                        field.setAccessible(false);
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                    return isNull;
+                })
+                .map(Field::getName)
+                .collect(Collectors.toList());
     }
 
     public static Map<String, List<String>> advancedValidate(Object obj) {
         Map<String, List<String>> result = new HashMap<>();
 
-        try {
-            Class<?> aClass = obj.getClass();
-            Field[] declaredFields = aClass.getDeclaredFields();
+        Class<?> aClass = obj.getClass();
 
-            for (Field field : declaredFields) {
-                field.setAccessible(true);
-                var listError = result.getOrDefault(field.getName(), new ArrayList<>());
-                if (field.isAnnotationPresent(NotNull.class) && Objects.isNull(field.get(obj))) {
-                    listError.add("can not be null");
-                    result.put(field.getName(), listError);
-                }
-                if (field.isAnnotationPresent(MinLength.class)) {
-                    MinLength minLengthInfo = field.getAnnotation(MinLength.class);
-                    if (Objects.isNull(field.get(obj))
-                            || field.get(obj).toString().length() < minLengthInfo.minLength()
-                    ) {
-                        listError.add("length less than " + minLengthInfo.minLength());
-                        result.put(field.getName(), listError);
+        List.of(aClass.getDeclaredFields()).stream()
+                .filter((field) -> field.isAnnotationPresent(NotNull.class)
+                        || field.isAnnotationPresent(MinLength.class))
+                .forEach(field -> {
+                    var fieldName = field.getName();
+                    var listErrors = getFieldErrors(field, obj);
+                    if (!listErrors.isEmpty()) {
+                        result.put(fieldName, listErrors);
                     }
+                });
 
-                }
-            }
+        return result;
+    }
+
+    public static List<String> getFieldErrors(Field field, Object obj) {
+        List<String> listErrors = new ArrayList<>();
+        String value = null;
+        try {
+            field.setAccessible(true);
+            value = (String) field.get(obj);
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
 
-        return result;
+        if (field.isAnnotationPresent(NotNull.class) && Objects.isNull(value)) {
+            listErrors.add("can not be null");
+        }
+        if (field.isAnnotationPresent(MinLength.class)) {
+            MinLength minLengthInfo = field.getAnnotation(MinLength.class);
+            if (Objects.isNull(value) || value.length() < minLengthInfo.minLength()
+            ) {
+                listErrors.add("length less than " + minLengthInfo.minLength());
+            }
+        }
+        return listErrors;
     }
 }
 // END
