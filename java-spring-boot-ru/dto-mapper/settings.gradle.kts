d609bf373a3c34d4997f757e4604c7ea3b336988
@hexlet/cli <support@hexlet.io>
2025-06-09 11:46:50 +0300

    submit java-spring-boot-ru/dto-mapper

---
+++ java-spring-boot-ru/dto-mapper/settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "mapper"
+// spring.jpa.generate-ddl = true
+// spring.jpa.hibernate.ddl-auto
---
+++ java-spring-boot-ru/dto-mapper/src/main/java/exercise/mapper/ProductMapper.java
@@ -0,0 +1,36 @@
+package exercise.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+import exercise.dto.ProductCreateDTO;
+import exercise.dto.ProductDTO;
+import exercise.dto.ProductUpdateDTO;
+import exercise.model.Product;
+
+
+// BEGIN
+@Mapper(
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class ProductMapper {
+    @Mapping(target = "name", source = "title")
+    @Mapping(target = "cost", source = "price")
+    @Mapping(target = "barcode", source = "vendorCode")
+    public abstract Product map(ProductCreateDTO dto);
+
+    @Mapping(target = "title", source = "name")
+    @Mapping(target = "price", source = "cost")
+    @Mapping(target = "vendorCode", source = "barcode")
+    public abstract ProductDTO map(Product model);
+
+    @Mapping(target = "cost", source = "price")
+    public abstract void update(ProductUpdateDTO dto, @MappingTarget Product model);
+}
+// END
