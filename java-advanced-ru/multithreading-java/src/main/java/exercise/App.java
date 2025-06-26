6f82482a63e28820a5ec02a97edee0b569d4b015
@hexlet/cli <support@hexlet.io>
2025-06-26 20:39:49 +0300

    submit java-advanced-ru/multithreading-java

--- java-advanced-ru/multithreading-java/src/main/java/exercise/App.java
+++ java-advanced-ru/multithreading-java/src/main/java/exercise/App.java
@@ -2,6 +2,7 @@ package exercise;
 
 import java.util.Map;
 import java.util.logging.Logger;
+import java.util.logging.Level;
 
 class App {
     private static final Logger LOGGER = Logger.getLogger("AppLogger");
@@ -11,13 +12,27 @@ class App {
         MaxThread maxThread = new MaxThread(numbers);
         MinThread minThread = new MinThread(numbers);
 
-        minThread.run();
-        maxThread.run();
+        minThread.start();
+        LOGGER.log(Level.INFO, "Thread " + minThread.getName() + " started");
+        maxThread.start();
+        LOGGER.log(Level.INFO, "Thread " + maxThread.getName() + " started");
+
+        try {
+            minThread.join();
+            LOGGER.log(Level.INFO, "Thread " + minThread.getName() + " finished");
+            maxThread.join();
+            LOGGER.log(Level.INFO, "Thread " + maxThread.getName() + " finished");
+        } catch (InterruptedException e) {
+            System.out.println("Поток был прерван");
+        }
 
         int min = minThread.getMin();
         int max = maxThread.getMax();
 
         Map<String, Integer> result = Map.of("min", min, "max", max);
+        
+        LOGGER.log(Level.INFO, "Result: " + result.toString());
+
         return result;
     }
     // END
